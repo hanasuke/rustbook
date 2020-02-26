e23f70ffb8d61e46627c8e280bec32fc610973cc
naosuke <info@naosuke.me>
2020-02-27 00:00:17 +0900

    :construction: study 3-5-6

--- 03/src/third.rs
+++ 03/src/third.rs
@@ -1,10 +1,11 @@
 use super::SortOrder;
+use std::cmp::Ordering;
 
 pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
   if x.len().is_power_of_two() {
     match *order {
-      SortOrder::Ascending =>  do_sort(x, true),
-      SortOrder::Descending =>  do_sort(x, false),
+      SortOrder::Ascending =>  sort_by(x, &|a, b| a.cmp(b)),
+      SortOrder::Descending =>  sort_by(x, &|a, b| b.cmp(a)),
     };
     Ok(())
   } else {
@@ -12,28 +13,50 @@ pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
   }
 }
 
-pub fn do_sort<T: Ord>(x: &mut [T], up: bool) {
+pub fn sort_by<T, F>(x: &mut [T], comparator: &F) -> Result<(), String>
+  where F: Fn(&T, &T) -> Ordering
+    {
+      if x.len().is_power_of_two() {
+        do_sort(x, true, comparator);
+        Ok(())
+      } else {
+        Err(format!("The length of x is not a powor of two. (x.len(): {}", x.len()))
+      }
+    }
+
+pub fn do_sort<T, F>(x: &mut [T], forward: bool, comparator: &F)
+    where F: Fn(&T, &T) -> Ordering
+{
   if x.len() > 1 {
     let mid_point = x.len() / 2;
-    do_sort(&mut x[..mid_point], true);
-    do_sort(&mut x[mid_point..], false);
-    sub_sort(x, up);
+    do_sort(&mut x[..mid_point], true, comparator);
+    do_sort(&mut x[mid_point..], false, comparator);
+    sub_sort(x, forward, comparator);
   }
 }
 
-fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
+fn sub_sort<T, F>(x: &mut [T],forward: bool, comparator: &F)
+  where F: Fn(&T, &T) -> Ordering
+{
   if x.len() > 1 {
-    compare_and_swap(x, up);
+    compare_and_swap(x, forward, comparator);
     let mid_point = x.len() / 2;
-    sub_sort(&mut x[..mid_point], up);
-    sub_sort(&mut x[mid_point..], up);
+    sub_sort(&mut x[..mid_point], forward, comparator);
+    sub_sort(&mut x[mid_point..], forward, comparator);
   }
 }
 
-fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
+fn compare_and_swap<T, F>(x: &mut [T], forward: bool, comparator: F)
+  where F: Fn(&T, &T) -> Ordering
+{
+    let swap_condition = if forward {
+      Ordering::Greater
+    } else {
+      Ordering::Less
+    };
   let mid_point = x.len() / 2;
   for i in 0..mid_point {
-    if ((x[i] > x[mid_point+i])) == up {
+    if comparator(&x[i], &x[mid_point+i]) == swap_condition {
       x.swap(i, mid_point+i);
     }
   }
@@ -41,9 +64,10 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
 
 #[cfg(test)]
 mod tests {
-  use super::sort;
+  use super::{sort, sort_by};
   use crate::SortOrder::*;
 
+  #[derive(Debug, PartialEq)]
   struct Student {
     first_name: String,
     last_name: String,
@@ -51,7 +75,7 @@ mod tests {
   }
 
   impl Student {
-    fn nes(first_name: &str, last_name: &str, age: u8) -> Self {
+    fn new(first_name: &str, last_name: &str, age: u8) -> Self {
       Self {
         first_name: first_name.to_string(),
         last_name: last_name.to_string(),
@@ -62,16 +86,16 @@ mod tests {
 
   #[test]
   fn sort_students_by_age_ascending() {
-    let taro = Studnet::new("taro", "yamada", 16)
-    let hanako = Studnet::new("hanako", "yamada", 14)
-    let kyoko = Studnet::new("kyoko", "ito", 15)
-    let ryosuke = Studnet::new("ryosuke", "hayashi", 17)
+    let taro = Student::new("taro", "yamada", 16);
+    let hanako = Student::new("hanako", "yamada", 14);
+    let kyoko = Student::new("kyoko", "ito", 15);
+    let ryosuke = Student::new("ryosuke", "hayashi", 17);
 
     let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
 
     let expected = vec![&hanako, &kyoko, &taro, &ryosuke];
     assert_eq!(
-      sort_by(&mut x, &|a, b| a.age.cmp(&b.age));
+      sort_by(&mut x, &|a, b| a.age.cmp(&b.age)),
       Ok(())
     );
 
@@ -80,16 +104,16 @@ mod tests {
 
   #[test]
   fn sort_students_by_name_ascending() {
-    let taro = Studnet::new("taro", "yamada", 16)
-    let hanako = Studnet::new("hanako", "yamada", 14)
-    let kyoko = Studnet::new("kyoko", "ito", 15)
-    let ryosuke = Studnet::new("ryosuke", "hayashi", 17)
+    let taro = Student::new("taro", "yamada", 16);
+    let hanako = Student::new("hanako", "yamada", 14);
+    let kyoko = Student::new("kyoko", "ito", 15);
+    let ryosuke = Student::new("ryosuke", "hayashi", 17);
 
     let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
     let expected = vec![&ryosuke, &kyoko, &hanako, &taro];
 
     assert_eq!(sort_by(&mut x, &|a, b| a.last_name.cmp(&b.last_name)
-                                .then_with(|| a.first_name.cmp(&b.firstname))), Ok(()));
+                                .then_with(|| a.first_name.cmp(&b.first_name))), Ok(()));
 
     assert_eq!(x, expected);
   }
