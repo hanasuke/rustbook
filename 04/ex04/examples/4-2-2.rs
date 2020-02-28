da70f3dfc95e550f5140e11621123e90aad33318
naosuke <info@naosuke.me>
2020-02-28 23:19:43 +0900

    :memo: study boolean type

---
+++ 04/ex04/examples/4-2-2.rs
@@ -0,0 +1,19 @@
+fn main() {
+  let b1 = true;
+  let b2 = !b1;
+
+  let n1 = 8;
+  let n2 = 12;
+
+  let b3 = n1 >= 10;
+  let b4 = n2 >= 10;
+  let b5 = b3 && b4;
+  let b6 = b3 || b4;
+
+  assert_eq!(b3, false);
+  assert_eq!(b4, true);
+  assert_eq!(b5, false);
+  assert_eq!(b6, true);
+
+  assert_eq!(std::mem::size_of::<bool>(), 1);
+}
